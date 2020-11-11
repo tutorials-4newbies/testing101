401f5a4ef2678c4649f7a49e684be4f8de203b7b
alonisser <alonisser@gmail.com>
2020-11-11 12:21:36 +0200

    more tests to implement

--- lib.py
+++ lib.py
@@ -10,5 +10,16 @@ class Validator:
         raise NotImplementedError("_validate Not implemented")
 
 
+VALID_PREFIXES = ["054", "055", "052", "053", "077"]
+
+
 class CellPhoneValidator(Validator):
-    pass
+    def _validate(self):
+
+        for char in self.val:
+            if not char.isdigit():
+                return False
+        prefix = self.val[0:3]
+        if prefix not in VALID_PREFIXES:
+            return False
+        return True
--- test_phone_validator.py
+++ test_phone_validator.py
@@ -2,16 +2,35 @@ import unittest
 
 from lib import CellPhoneValidator
 
-
+# This is the spec!
 class PhoneValidatorTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
 
     def test_phone_validator_regular_number(self):
         value = "0546734469"
-        result = CellPhoneValidator(value).is_valid()
-        self.assertTrue(result)
+        expected_true_result = CellPhoneValidator(value).is_valid()
+        self.assertTrue(expected_true_result)
 
+        false_value = "aaaaa"
+        expected_false_result = CellPhoneValidator(false_value).is_valid()
+        self.assertFalse(expected_false_result)
+
+    def test_cell_phone_valid_prefix(self):
+        value = "0554446666"
+        expected_true_result = CellPhoneValidator(value).is_valid()
+        self.assertTrue(expected_true_result)
+
+        false_value = "0114446666"
+        expected_false_result = CellPhoneValidator(false_value).is_valid()
+        self.assertFalse(expected_false_result)
+
+    def test_cell_phone_prefix_without_zero(self):
+        pass
+
+    def test_cell_phone_has_legal_number_of_numbers(self):
+        pass
+
+    def test_cell_phone_number_can_contain_spaces_and_dashes(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
