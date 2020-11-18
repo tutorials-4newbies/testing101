bfa3740fb07e670573b203e01a35e7441c47adb9
alonisser <alonisser@gmail.com>
2020-11-18 18:03:09 +0200

    A di variation

--- test_open_hours.py
+++ test_open_hours.py
@@ -1,24 +1,40 @@
+import datetime
 import unittest
 
 from lib import OpeningHoursHelper
-from freezegun import freeze_time
+
+
+# Can we do it cleaner? can we utilize Object oriented programming to get rid of this endless mocking?
+class TimeProvider:
+    def __init__(self):
+        self.frozen_time = False
+
+    def now(self):
+        if not self.frozen_time:
+            return datetime.datetime.now()
+
+        else:
+            return self.frozen_time
+
+    def freeze_time(self, frozen_datetime: datetime.datetime):
+        self.frozen_time = frozen_datetime
+
+    def release_time(self):
+        self.frozen_time = False
 
 
 # This is the spec!
 class OpenHoursHelperTestCase(unittest.TestCase):
 
-    @freeze_time("2020-11-15T08:30")
     def test_opening_hours_on_sunday_morning(self):
-        helper = OpeningHoursHelper()
+        # Dependency injection
+
+        time_provider = TimeProvider()
+        time_provider.freeze_time(datetime.datetime(2020, 11, 15, 8, 30))
+        helper = OpeningHoursHelper(time_provider)
         is_open = helper.is_it_open_now()
 
         self.assertTrue(is_open)
-
-# add the following tests
-# test it's closed before 8 on sunday
-# test it's closed after 16 on sunday
-# test it's closed every hour in non workdays
-# test tuesday behavior
 
 if __name__ == '__main__':
     unittest.main()
