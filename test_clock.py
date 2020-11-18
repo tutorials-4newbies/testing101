f4f3a5d70b316aa5a45640982b29f02a2ff1c243
alonisser <alonisser@gmail.com>
2020-11-18 16:47:46 +0200

    with question

--- test_clock.py
+++ test_clock.py
@@ -3,15 +3,35 @@ import pytest
 from clock import Clock
 
 
-def test_api_response_is_checking_tea_time_correctly():
+def test_api_response_is_checking_tea_time_correctly(mocker):
+    def mock_get(*args, **kwargs):
+        class MockResponse:
+            status_code = 200
+
+            def json(self):
+                return {
+                    "currentDateTime": "2020-11-18T15:00-05:00"
+                }
+
+        return MockResponse()
+
+    mocker.patch("clock.requests.get", mock_get)
 
     res = Clock.now("est")
     expected_response = "it's 15:00 o'clock in est, teatime!"
     assert res == expected_response, f"Expected response to be {expected_response} but was different: {res}"
 
 
-# add a test for checking that in another time zone it's not a time for tea
+# Now, since the api is costly, we want to cache the results, and the tests should verify it's actually caching
 
-# add a test and then a feature where israel time zone is never time for tea
+def test_api_is_not_being_called_twice_for_specific_timezone(mocker):
+    res = Clock.now("est")
+    expected_response = "it's 15:00 o'clock in est, teatime!"
+
+    res = Clock.now("est")
+    expected_response = "it's 15:00 o'clock in est, teatime!"
+
+    # Hmm but how can we verify that it's not called twice? 
+
 if __name__ == '__main__':
     pytest.main()
