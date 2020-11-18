080ec3f36405f74c822a01fc984ab0e9f9b69f6b
alonisser <alonisser@gmail.com>
2020-11-18 15:05:53 +0200

    annotated questions

--- clock.py
+++ clock.py
@@ -9,6 +9,7 @@ class Clock:
 
     @staticmethod
     def _get_time(timezone: str):
+        # what's the problem here?
         res = requests.get(f"http://worldclockapi.com/api/json/{timezone}/now")
         data = res.json()
 
